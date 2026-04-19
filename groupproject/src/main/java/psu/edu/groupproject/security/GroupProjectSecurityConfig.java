26b8c69fc3daf2520e50f287be3dc95277491ecf
JustinPerera <pererajustin5@gmail.com>
2026-04-19 14:02:12 -0400

    Update GroupProjectSecurityConfig.java

--- groupproject/src/main/java/psu/edu/groupproject/security/GroupProjectSecurityConfig.java
+++ groupproject/src/main/java/psu/edu/groupproject/security/GroupProjectSecurityConfig.java
@@ -4,18 +4,50 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-// takes the users from the database and has them as credentials
 @Configuration
 public class GroupProjectSecurityConfig {
 
-
     @Bean
-    public UserDetailsManager userDetailsManger(DataSource dataSource) {
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
     }
-    
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+            configurer
+                .requestMatchers("/").hasAnyRole("SUPERVISOR", "MANAGER", "ADMIN")
+                .requestMatchers("/employees/list").hasAnyRole("EMPLOYEE", "MANAGER", "ADMIN")
+                .requestMatchers("/employees/showFormForAdd").hasAnyRole("MANAGER", "ADMIN")
+                .requestMatchers("/employees/showFormForUpdate").hasAnyRole("MANAGER", "ADMIN")
+                .requestMatchers("/employees/save").hasAnyRole("MANAGER", "ADMIN")
+                .requestMatchers("/employees/delete").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        );
+
+        http.formLogin(form ->
+            form
+                .loginPage("/showLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll()
+        );
+
+        http.logout(logout ->
+            logout
+                .permitAll()
+        );
+
+        http.exceptionHandling(configurer ->
+            configurer
+                .accessDeniedPage("/access-denied")
+        );
+
+        return http.build();
+    }
 }

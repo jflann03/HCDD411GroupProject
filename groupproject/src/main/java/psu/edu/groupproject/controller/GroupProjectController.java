8cbcd2c7cf4e9bb545d6492df2d45724f3d9d83c
RyanColby20 <Ryanwculbertson@verizon.net>
2026-04-18 22:12:50 -0400

    Fixing various issues with GroupProjectController and Vibe changing new employee-form.html to fit the new Controller

--- groupproject/src/main/java/psu/edu/groupproject/controller/GroupProjectController.java
+++ groupproject/src/main/java/psu/edu/groupproject/controller/GroupProjectController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
 import psu.edu.groupproject.entity.Person;
 import psu.edu.groupproject.service.EmployeeServiceImpl;
 
@@ -57,8 +59,10 @@ public class GroupProjectController {
     }
 
     @PostMapping("/save")
-    public String saveEmployee(@ModelAttribute("employee") Person employee) {
-
+    public String saveEmployee(@Valid @ModelAttribute("employee") Person employee, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "employee-form";
+        }
         employeeServiceImpl.save(employee);
 
         return "redirect:/employees/list";
